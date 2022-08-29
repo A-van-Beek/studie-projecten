2c2cb82e70c9cf14d9332551713fd09cac76364e
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-08-29 15:42:41 +0200

    intro namespace

--- 04_Namespace/app.js
+++ 04_Namespace/app.js
@@ -1,36 +1,48 @@
+// oorspronkelijke global code, zie: app-global !!
+/*
+dit wordt opgelost door een object te maken in de global,
+met daarbinnen de variablen.
 
-var prompt = "Welcome,",
-    prompt2 = "How are you?",
-    prompt3 = "Good to see you again.",
-    counter = 0,
-    users = [];
+let op: aanroepen in console met MYAPP.greeting("mary")
+*/
 
-var greeting = function(user){
-    var greeting = prompt + " ";
-    if (newUser(user)) {
-        greeting += prompt2;
-    } else {
-        greeting += prompt3;
-    }
-    console.log(greeting);
-    counter++;
-};
+var MYAPP = MYAPP || {};
+        // nieuwe variabele = de oude variabel, of nieuw object
 
-var newUser = function(user) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].toUpperCase() === user.toUpperCase()) {
-            return false;
+(function(namespace) {
+    var prompt = "Welcome,",
+        prompt2 = "How are you?",
+        prompt3 = "Good to see you again.",
+        counter = 0,
+        users = [];
+
+    namespace.greeting = function(user){ //deze is public
+        var greeting = prompt + " ";
+        if (newUser(user)) {
+            greeting += prompt2;
+        } else {
+            greeting += prompt3;
         }
-    }
-    addUser(user);
-    return true;
-};
+        console.log(greeting);
+        counter++;
+    };
 
-var addUser = function(user) {
-    users.push(user);
-};
+    var newUser = function(user) { //deze is private
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].toUpperCase() === user.toUpperCase()) {
+                return false;
+            }
+        }
+        namespace.addUser(user);
+        return true;
+    };
 
-var numberOfGreetings = function() {
-    console.log("Total number of greetings: " + counter);
-};
+    namespace.addUser = function(user) {
+        users.push(user);
+    };
+
+    namespace.numberOfGreetings = function() {
+        console.log("Total number of greetings: " + counter);
+    };
+})(MYAPP);
 
