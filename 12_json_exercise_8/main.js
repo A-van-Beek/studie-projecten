3bfbf522c304b2775dba06fd3f8995eb06af2f52
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-09-06 13:50:31 +0200

    aantekeningen

--- 12_json_exercise_8/main.js
+++ 12_json_exercise_8/main.js
@@ -5,25 +5,30 @@
 
 var MAINAPP = (function(app) {
     var jsonObj = {},
-        doc =document;
+        doc = document;
 
     var loadJSON = function(path) {
         var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType('application/json');
-        xobj.open('GET', path);
-        xobj.onreadystatechange = function() {
-            if (xobj.readyState === 4) {
-                jsonObj = JSON.parse(xobj.responseText);
-                populateHtml();
+        xobj.overrideMimeType('application/json'); //aanpassen mime type
+        xobj.open('GET', path); //open the object
+        xobj.onreadystatechange = function() { //wachten tot geladen is.. (=callbackfie)
+            if (xobj.readyState === 4) { //als deze klaar is (uitslag 4):
+                jsonObj = JSON.parse(xobj.responseText); //maak het object af.
+                populateHtml(); //dit is de tweede functie die aangeroepen wordt
             }
         };
         xobj.send(null);
     };
 
     var populateHtml = function() {
-        var li = doc.getElementsByTagName('li');
-        doc.getElementsByTagName('h2')[0].innerHTML = jsonObj.heading;
+        var li = doc.getElementsByTagName('li'); //zoek alle elementen by tagname (li)
+        // var tmp = doc.getElementsByTagName('h2') //dit is een nodelist (HTMLCollection)!
+        // console.log(tmp)
+
+        doc.getElementsByTagName('h2')[0].innerHTML = jsonObj.heading; //geef de juiste innderHTML
+        
         for (let i = 0;i < li.length;i++) {
+            console.log(li)
             li[i].innerHTML = jsonObj.bullets['b' + (i + 1)];
         }
     };
