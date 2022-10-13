3243bcde03dac39b5e4dd05b085f556ba60db30b
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-10-13 11:27:12 +0200

    Add values to leaf nodes

--- mobx-eigen/src/App.js
+++ mobx-eigen/src/App.js
@@ -1,14 +1,13 @@
 import { observable, action } from "mobx";
 import Tree from "./Tree";
 import List from "./List";
+import Values from "./Values";
 import "./App.css";
 
-export default function App() {
+function App() {
   const tree = observable([]);
-
   const addElement = action(() => {
     const count = tree.length;
-
     const newItem = {
       title: `Item ${count}`,
       children: [
@@ -17,12 +16,14 @@ export default function App() {
           children: [
             {
               title: `Item ${count}.A.I`,
+              value: "Hello, world! (0)",
             },
             {
               title: `Item ${count}.A.II`,
               children: [
                 {
                   title: `Item ${count}.A.II.a`,
+                  value: "Hello, world! (1)",
                 },
               ],
             },
@@ -30,17 +31,16 @@ export default function App() {
         },
         {
           title: `Item ${count}.B`,
+          value: "Hello, world! (2)",
         },
       ],
     };
 
     tree.push(newItem);
   });
-
   const toggleExpanded = action((item) => {
     item.expanded = !item.expanded;
   });
-
   const toggleSelected = action((item) => {
     item.selected = !item.selected;
     const updateChildren = (item) => {
@@ -49,20 +49,15 @@ export default function App() {
         updateChildren(child);
       });
     };
-
     updateChildren(item);
-
     const parents = new Map();
-
     const findParents = (item) => {
       item.children?.forEach((child) => {
         parents.set(child, item);
         findParents(child);
       });
     };
-
     tree.forEach(findParents);
-
     let parent = parents.get(item);
     while (parent) {
       if (!item.selected && parent.children.find((child) => child.selected)) {
@@ -72,7 +67,6 @@ export default function App() {
       parent = parents.get(parent);
     }
   });
-
   return (
     <div className="App">
       <h1>Demo MobX Tree</h1>
@@ -85,6 +79,10 @@ export default function App() {
       />
       <h2>selected</h2>
       <List data={tree} />
+      <h2>values</h2>
+      <Values data={tree} />
     </div>
   );
 }
+
+export default App;
