e8444200357519ba4da60bf59742067e33f2bae6
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-10-13 11:55:31 +0200

    Prevent recalculation of parent map

--- mobx-eigen/src/TreeStore.js
+++ mobx-eigen/src/TreeStore.js
@@ -1,4 +1,28 @@
-import { makeObservable, observable, action } from "mobx";
+import { makeObservable, observable, action, computed } from "mobx";
+
+class TreeItem {
+    title = null;
+    value = null;
+    children = null;
+
+    selected = false;
+    expanded = false;
+
+    constructor(title, value, children) {
+        makeObservable(this, {
+            title: observable,
+            value: observable,
+            children: observable,
+
+            selected: observable,
+            expanded: observable
+        });
+
+        this.title = title;
+        this.value = value;
+        this.children = children;
+    }
+}
 
 export default class TreeStore {
   value = [];
@@ -6,14 +30,33 @@ export default class TreeStore {
   constructor() {
     makeObservable(this, {
       value: observable,
+
       reset: action,
       addElement: action,
       setSelectedAll: action,
       setExpandedAll: action,
       toggleExpanded: action,
       toggleSelected: action,
+
+    // kept alive in case only used in actions
+       parents: computed({keepAlive: true}),
     });
   }
+
+  get parents() {
+    const map = new Map();
+
+    const findParents = (item) => {
+    item.children?.forEach(child => {
+        map.set(child, item);
+        findParents(child);
+    });
+    };
+
+    this.value.forEach(findParents);
+
+    return map;
+}
 
   reset() {
     this.value.splice(0);
@@ -22,33 +65,15 @@ export default class TreeStore {
   addElement() {
     const count = this.value.length;
 
-    const newItem = {
-      title: `Item ${count}`,
-      children: [
-        {
-          title: `Item ${count}.A`,
-          children: [
-            {
-              title: `Item ${count}.A.I`,
-              value: "Hello, world! (0)",
-            },
-            {
-              title: `Item ${count}.A.II`,
-              children: [
-                {
-                  title: `Item ${count}.A.II.a`,
-                  value: "Hello, world! (1)",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          title: `Item ${count}.B`,
-          value: "Hello, world! (2)",
-        },
-      ],
-    };
+    const newItem = new TreeItem(`Item ${count}`, null, [
+        new TreeItem(`Item ${count}.A`, null, [
+            new TreeItem(`Item ${count}.A.I`, "Hello, world! (0)"),
+            new TreeItem(`Item ${count}.A.II`, null, [
+                new TreeItem(`Item ${count}.A.II.a`, "Hello, world! (1)")
+            ])
+        ]),
+        new TreeItem(`Item ${count}.B`, "Hello, world! (2)")
+    ]);
 
     this.value.push(newItem);
   }
@@ -67,7 +92,11 @@ export default class TreeStore {
   }
 
   setExpandedAll(value) {
-    this.forAllItems((item) => (item.expanded = value));
+    this.forAllItems(item => {
+        if (item.children) {
+            item.expanded = value;
+        }
+    });
   }
 
   toggleExpanded(item) {
@@ -103,7 +132,7 @@ export default class TreeStore {
         break;
       }
       parent.selected = item.selected;
-      parent = parents.get(parent);
+      parent = this.parents.get(parent);
     }
   }
 }
