6a48e2d700d8a7e141d843ba252a15756593f3fe
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-10-13 11:14:10 +0200

    implement tree component

--- mobx-eigen/src/App.js
+++ mobx-eigen/src/App.js
@@ -1,11 +1,58 @@
-import './App.css';
+import { observable, action } from "mobx";
+import Tree from "./Tree";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const tree = observable([]);
+
+  const addElement = action(() => {
+    const count = tree.length;
+
+    const newItem = {
+      title: `Item ${count}`,
+      children: [
+        {
+          title: `Item ${count}.A`,
+          children: [
+            {
+              title: `Item ${count}.A.I`,
+            },
+            {
+              title: `Item ${count}.A.II`,
+              children: [
+                {
+                  title: `Item ${count}.A.II.a`,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: `Item ${count}.B`,
+        },
+      ],
+    };
+
+    tree.push(newItem);
+  });
+
+  const toggleExpanded = action((item) => {
+    item.expanded = !item.expanded;
+  });
+
+  const toggleSelected = action((item) => {
+    item.selected = !item.selected;
+  });
+
   return (
     <div className="App">
-      Hello, world !
+      <h1>Demo MobX Tree</h1>
+      <Tree
+        data={tree}
+        toggleExpanded={toggleExpanded}
+        toggleSelected={toggleSelected}
+      />
+      <button onClick={addElement}>Add element</button>
     </div>
   );
 }
-
-export default App;
