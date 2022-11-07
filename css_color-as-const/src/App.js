3fc1ec265dad1a9b4b06b3e555defca2444661b3
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2022-11-07 13:37:24 +0100

    init

---
+++ css_color-as-const/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import './colors.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Verschillende snippets.
+    </div>
+
+<div className="test">
+deze achtergrond-kleur is voorgedefinieerd in een aparte file.
+</div>
+<div className="blok-1">1</div>
+<div className="blok-2">2</div>
+<div className="blok-3">3</div>
+<div className="blok-4">4</div>
+</div>
+
+  );
+}
+
+export default App;
