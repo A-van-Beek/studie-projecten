2b96c6f2459f6e6822154fc6391bccb495f462fd
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2023-01-13 11:18:47 +0100

    probeersel 1

--- react_controlled_form/src/components/Form.js
+++ react_controlled_form/src/components/Form.js
@@ -1,14 +1,42 @@
+import React, { useState } from "react";
+import employees from "../data";
+
+/** 
+ * probleem blijft de submit-knop
+ * ik probeer even een andere website...
+*/
+
 function Form() {
-  const contentForm = (
-    <form>
-      <label>
-        Name:
-        <input type="text" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+  const [query, setQuery] = useState("");
+
+  const onChange = (event) => setQuery(event.target.value);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log("hier", query);
+//   }
+
+  const handleSubmit = () => {
+    console.log("hier", query);
+  }
+
+  const filteredEmployees = employees.filter((name) => {
+    return name.toLocaleLowerCase().includes(query.toLocaleLowerCase());
+  });
+
+
+  return (
+    <>
+    <div>Input value: {query}</div>
+    <input placeholder="Type a value" value={query} onChange={onChange} />
+    <input type="submit" value="Submit !" onSubmit={handleSubmit()}/>
+    <div className="list">
+        {filteredEmployees.map((name, idx) => (
+          <div key={idx}>{name}</div>
+        ))}
+      </div>
+  </>
   );
-  return contentForm;
 }
 
 export default Form;
