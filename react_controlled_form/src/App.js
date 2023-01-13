c836bb777181acf12594d274890be260bdbd3391
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2023-01-13 09:45:50 +0100

    delete redundant info + add simple form

--- react_controlled_form/src/App.js
+++ react_controlled_form/src/App.js
@@ -1,9 +1,25 @@
-import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import Form from "./components/Form";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+  ];
+
   return (
-    <div className="App">
-      <h1>basis</h1>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <Form/>
     </div>
   );
 }
---
+++ react_controlled_form/src/components/Form.js
@@ -0,0 +1,14 @@
+function Form() {
+  const contentForm = (
+    <form>
+      <label>
+        Name:
+        <input type="text" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+  return contentForm;
+}
+
+export default Form;
